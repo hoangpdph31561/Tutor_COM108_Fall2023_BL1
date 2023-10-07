d91c22f131307f01842f046c8d1cb145a74bf76d
hoangpdph31561 <hoangpdph31561@fpt.edu.vn>
2023-10-08 01:53:43 +0700

    demo project

---
+++ Source_code/Demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello word\n");
+	int a;
+	printf("Moi nhap so a: ");
+	scanf("%d",&a);
+	printf("So vua nhap la: %d\n",a);
+	float b;
+	printf("Moi nhap so b: ");
+	scanf("%f",&b);
+	printf("So b vua nhap la: %.2f\n",b);
+	return 0;
+}
+
+
